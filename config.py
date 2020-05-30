57735bdf299206595a7ca25e6ffb341ebd7530f1
blek3425 <64996015+blek3425@users.noreply.github.com>
2020-05-30 13:55:32 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+## LOGIN
+Username = 'xxx'
+Password = 'xxx'
+##BET SETTINGS SILAKAN ATUR SENDIRI
+base_bet = float(0.010)
+if_lose = float(1.2)
+chance1 = int(7) ##isi sama jika chance tidak random
+chance2 = int(15) ##isi sama jika chance tidak random
+target_profit = float(0.1)
+interval = float(0.1) ##second
+#TOOLS
+#otomatis refresh setelah mencapai target profit
+autorefresh = "ON"#ON/OFF || REFRESH SETIAP PROFIT TERPENUHI
+#otomatis withdraw setelah ifbalance
+autowithdraw = "OFF"#ON/OFF
+ifbalance = int(42)
+address = "DEq3PSYgjvHYgXALxME6XU33C4K3nVgc4S"
+amount = 10
